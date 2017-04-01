feac99dac7ced4b675690803fa35c4a3dad26817
Saul Greene <spdysg@gmail.com>
2017-04-01 16:20:36 -0700

    github API working

--- public/scripts/models/projectModel.js
+++ public/scripts/models/projectModel.js
@@ -2,7 +2,6 @@
 
 (function(module){
 
-//projects constructor
   function Project(argument){
     this.title = argument.title;
     this.description = argument.description;
@@ -11,18 +10,14 @@
     this.link = argument.link;
     this.img = argument.img;
   }
-  //projects page initialization, appending each project to the dom //
   Project.all = [];
 
-  //project Handlebars  rendering
   Project.prototype.toHtml = function() {
     var source = $('#template').text();
     var templateRender = Handlebars.compile(source);
     return templateRender(this);
   }
 
-
-//fetch all goes through the JSON file and looks for the project information at which point it calls loadAll which pushes each JSON object through the constructor and then runs the hide all and show this ID functionability of the nav-bar
   Project.fetchAll = function() {
     if (localStorage.projectData) {
       Project.loadAll(JSON.parse(localStorage.projectData));
@@ -35,7 +30,7 @@
       });
     }
   }
-//this pushes each project through the constructor and then hides all and shows the page with the matching ID of what was clicked.
+
   Project.loadAll = function(projectData) {
       Project.all = projectData.map(function(input){
         return new Project(input);
--- server.js
+++ server.js
@@ -1,18 +1,12 @@
 'use strict';
 
-// Load Express
 const express = require('express');
 const requestProxy = require('express-request-proxy');
-// Instantiate Express so that we can use its functionality
 const app = express();
-
-// Designate a port to serve our app on
 const PORT = process.env.PORT ||  3000;
 
-// Define which directory we will serve static files from
 app.use(express.static('./public'));
 
-// Here is one way to set up a route... bats...
 function proxyGitHub(request, response) {
   console.log('Routing GitHub request for', request.params[0]);
   (requestProxy({
@@ -21,7 +15,7 @@ function proxyGitHub(request, response) {
   }))(request, response);
 }
 
-// REVIEW: This is a new route that will utilize our middle man proxy.
+
 app.get('/github/*', proxyGitHub);
 app.listen(PORT, function(){
   console.log('Your app is served on local host 3000');
